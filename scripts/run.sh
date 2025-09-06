5ea59f638e56f50c42021cddf1f4bc378216fc4f
Brennan Davenport <brennandavenport9@gmail.com>
2025-09-06 18:51:07 -0400

    added dynamic shell generation

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,2 @@
-    #!/bin/bash
-
-    # adjust the port based on what is being exposed in the main dockerfile
-    docker run -p 10000:10000 --rm -v "$(pwd)":/workspace exchange_backend_image \
-    bash -c "./build/apps/exchange-server/exchange_backend"
+#!/bin/bash
+bash build/generated/scripts/run.sh
