f1f4494d2513f59dab8679f21e82f0532aaecc8a
vkatari10 <kaskat2021@gmail.com>
2025-10-24 17:28:58 -0400

    feat: Added skeleton for network manager

---
+++ include/network-manager/network_manager.hpp
@@ -0,0 +1,15 @@
+/**
+ * @brief Network Manager to handle all inbound and outbound network connections
+*/
+
+#include "network-inbound/api_routing.hpp"
+#include "network-inbound/data_conversion.hpp"
+#include "network-inbound/data_validation.hpp"
+
+// Also include network outbound headers when they are complete
+
+/**
+ * ====Responsibilities====
+ * - Start inbound network server
+ * - Start outbound network server 
+ */
