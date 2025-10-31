e6891d0bd075c1e0b10e60e2f8d6e6d63ce01e3c
vkatari10 <kaskat2021@gmail.com>
2025-10-31 15:49:55 -0400

    feat: Added order removal method

--- include/shared/orderbook.hpp
+++ include/shared/orderbook.hpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <deque>
 #include <cstdint>
+#include <optional>
+#include <vector>
 
 #include "shared/order.hpp"
 
@@ -14,51 +16,74 @@ class OrderBook {
 public:
 
     /**
-     * @brief Adds an order to the OrderBook
+     * @brief adds an order to the OrderBook
      * 
      * @param order the order being added
-     * @return 1 if the order was successfully added to the orderbook
      */
-    uint8_t push_order(exchange::Order& order) { // Maybe change this to return bool to ensure it can be checked? Else this can just be void 
+    void push_order(exchange::Order& order) { // Maybe change this to return bool to ensure it can be checked? Else this can just be void 
         this->orderbook_[order.price].push_back(order);
-        return 1;
     }
 
     /**
-     * @brief Removes the first pushed order from a given price level 
+     * @brief removes the first pushed order from a given price level 
      * 
      * @param price_level the price level to remove the order from 
      * @return 1 if the order was successfully removed from the order, else 0 if there 
      * was no orders at that price level
      */
-    exchange::Order pop_order(uint64_t price_level) { 
+    std::optional<exchange::Order> pop_order(uint64_t price_level) { 
         if (this->orderbook_[price_level].size() > 0) {
             exchange::Order order = this->orderbook_[price_level].front();
             this->orderbook_[price_level].pop_front(); 
             return order;
         } else {
-
+            return std::nullopt;
         }
     }
 
     /**
-     * @brief Removes a given order from the orderbook entirely
+     * @brief removes a given order from the OrderBook entirely
      * 
      * @param order the order to remove
      * @return 1 if the order was able to be removed, else 0
      */
-    uint8_t remove_order(exchange::Order& order) {
+    std::optional<exchange::Order> remove_order(exchange::Order& order) { // This method O(N) because we using std::deque
+        u_int64_t count = 0;
+        u_int64_t deque_size = (u_int64_t) this->orderbook_[order.price].size();
+        for (exchange::Order check : this->orderbook_[order.price]) {
+            if (check.order_id == order.order_id) { 
+                auto ele = this->orderbook_[order.price].begin() + count;
+                this->orderbook_[order.price].erase(ele);
+            }
+            ++count;
+        }
 
-        // for (or : this->orderbook_[order.price]) {
+        if (count == deque_size) {
+            return std::nullopt;
+        }
 
-        // }
-
+        return order; 
     }
 
+    /**
+     * @brief modifies a given order in the OrderBook 
+     * 
+     * @param order the order to modify
+     */
+    void modify_order(exchange::Order& order) {
+        this->remove_order(order);
+        this->push_order(order);
+    }
+    
 private:
-
+    // main order book
     std::map<u_int64_t, std::deque<exchange::Order>> orderbook_;
 
+    // map from client to orders in the order book so removal can be faster
+    // we can change the container to be something else instead of just a vector 
+
+    // not being used yet
+    std::map<uint64_t, std::vector<exchange::Order>> user_orders_;
 };
 }
 
