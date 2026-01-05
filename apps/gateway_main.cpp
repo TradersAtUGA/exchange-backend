7be12b1c356456c0e19cfb28d393a045154b1847
vkatari10 <kaskat2021@gmail.com>
2026-01-05 14:48:44 -0500

    feat: added gateway main

--- apps/gateway_main.cpp
+++ apps/gateway_main.cpp
@@ -1,1 +1,25 @@
-// TO DO create executable to spin up FIX/TCP connections
+#include <iostream>
+
+#include "gateway/gateway.hpp"
+
+#include "quickfix/SocketAcceptor.h"
+#include "quickfix/FileStore.h"
+#include "quickfix/FileLog.h"
+#include "quickfix/SessionSettings.h"
+
+// starts gateway to accept incoming orders from brokers
+int main() {
+
+    FIX::SessionSettings settings("gateway.cfg");
+    Gateway app;
+    FIX::FileStoreFactory storeFactory(settings);
+    FIX::FileLogFactory logFactory(settings);
+    FIX::SocketAcceptor acceptor(app, storeFactory, settings, logFactory);
+
+    acceptor.start();
+    std::cout << "Exchange acceptor running..." << "\n";
+    std::cout << "press ENTER to stop" << "\n";
+    std::cin.get();  // wait for Enter
+    acceptor.stop();
+    return 0;
+}
