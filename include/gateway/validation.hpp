5db098f79ad29f44c8d82ba6a47fce58aef4bbcb
vkatari10 <kaskat2021@gmail.com>
2026-01-07 11:34:22 -0500

    feat: Added constexpr lookup arrays

--- include/gateway/validation.hpp
+++ include/gateway/validation.hpp
@@ -3,7 +3,7 @@
 
 #include <cstdint>
 #include <string>
-#include <unordered_map>
+#include <array>
 
 #include "shared/order.hpp"
 #include "shared/enums.hpp"
@@ -15,43 +15,93 @@ namespace exchange {
 /*
 Dispatch tables to lookup valid values and conversions
 into internal enums
+
+Note that fix values are char values and some fix 
+valuse begin at '1' or '0'. Depending on indexing 
+some arrays are accessed by ['1' - '0' - 1] if they 
+start at 1 and ['1' - '0'] if they start at 0. 
 */
 
-const std::unordered_map<char, Side> VALID_SIDES = {
-{FIX::Side_BUY, Side::BUY}, 
-{FIX::Side_SELL, Side::SELL},
-};
+// NOTE: fix values start at 1!
+inline constexpr std::array<uint8_t, 17> VALID_SIDES = [] {
+    std::array<uint8_t, 17> arr; 
+    arr[0] = 1; // Side::BUY
+    arr[1] = 1; // Side::SELL
+    for (size_t i{ 2 }; i < 17; ++i) {
+        arr[i] = 0;
+    }
+    return arr;
+}();
 
-const std::unordered_map<char, OrderType> VALID_ORDER_TYPES = {
-    {FIX::OrdType_MARKET, OrderType::MARKET},
-    {FIX::OrdType_LIMIT, OrderType::LIMIT},
-};
+// NOTE: fix values DO NOT start at 1!
+inline constexpr std::array<uint8_t, 10> VALID_TIF = [] {
+    std::array<uint8_t, 10> arr; 
+    arr[0] = 1; // TIF::DAY
+    arr[1] = 0; // GTC (not impl)
+    arr[2] = 0; // at the open (not impl)
+    arr[3] = 1; // IOC
+    arr[4] = 1; // FOK 
+    for (size_t i{ 5 }; i < 10; ++i) {
+        arr[i] = 0;
+    }
+    return arr;
+}();
 
-const std::unordered_map<char, TIF> VALID_TIF = {
-    {FIX::TimeInForce_DAY, TIF::DAY}, 
-    {FIX::TimeInForce_IMMEDIATE_OR_CANCEL, TIF::IOC}, 
-    {FIX::TimeInForce_FILL_OR_KILL, TIF::FOK},
-};
+// NOTE: fix values BEGIN at 1!
+inline constexpr std::array<uint8_t, 30> VALID_ORDER_TYPES = [] {
+    std::array<uint8_t, 30> arr; 
+    arr[0] = 1; // market
+    arr[1] = 1; // limit
+    for (size_t i{ 2 }; i < 30; ++i) {
+        arr[i] = 0;
+    }
+    return arr;
+}();
+
+
+// NOTE: fix values start at 1!
+inline constexpr std::array<Side, 2> SIDE_MAP = [] {
+    std::array<Side, 2> arr;
+    arr[0] = Side::BUY; // FIX::Side_BUY = '1'
+    arr[1] = Side::SELL; // '2'
+    return arr;
+}();
+
+// NOTE: fix values start at 1!
+inline constexpr std::array<OrderType, 2> ORD_TYPE_MAP = [] {
+    std::array<OrderType, 2> arr;
+    arr[0] = OrderType::MARKET;
+    arr[1] = OrderType::LIMIT;
+    return arr;
+}();
+
+// NOTE: fix values DO NOT start at 1!
+inline constexpr std::array<TIF, 5> TIF_MAP = [] {
+    std::array<TIF, 5> arr; 
+    arr[0] = TIF::DAY;
+    // note that orders are valid at this point
+    // since they have already been validated
+    // by previous arrays
+    arr[1] = TIF::DAY; // actually GTC
+    arr[2] = TIF::DAY; // actually at the open 
+    arr[3] = TIF::IOC;
+    arr[4] = TIF::FOK;
+    return arr;
+}();
 
 /** @brief looks up in valid side dispatch table if valid side provided */
 inline static uint8_t validate_side(char fix_value) {
-    
-    if (auto it = VALID_SIDES.find(fix_value); it != VALID_SIDES.end()) return true;
-    return false;
+    return VALID_SIDES[static_cast<size_t>(fix_value - '0' - 1)] == 1;
 }
 
 /** @brief looks up in valid order type dispatch table if valid order type was provided */
 inline static uint8_t validate_order_type(char fix_value) {
-  
-    if (auto it = VALID_ORDER_TYPES.find(fix_value); it != VALID_ORDER_TYPES.end()) return true;
-    return false;
+    return VALID_ORDER_TYPES[static_cast<size_t>(fix_value - '0' - 1)] == 1;
 }
 
 /** @brief looks up in the valid tif dispatch table if valid tif provided */
 inline static uint8_t validate_tif(char fix_value) {
-     
-    if (auto it = VALID_TIF.find(fix_value); it != VALID_TIF.end()) return true;
-    return false;
+    return VALID_TIF[static_cast<size_t>(fix_value - '0')] == 1;
 }
 
 /** @brief creates a new order given that its valid */
@@ -65,9 +115,9 @@ inline static exchange::Order generate_order(
 ) {
     exchange::Order o; 
     // FIX provided fields 
-    o.side = VALID_SIDES.at(side);
-    o.order_type = VALID_ORDER_TYPES.at(order_type);
-    o.tif = VALID_TIF.at(tif);
+    o.side = SIDE_MAP[static_cast<size_t>(side - '0' - 1)];
+    o.order_type = ORD_TYPE_MAP[static_cast<size_t>(side - '0' - 1)];
+    o.tif = TIF_MAP[static_cast<size_t>(side - '0')];
     o.price = exchange::double_to_uint64_t(price);
     o.qty = static_cast<uint64_t>(qty); // avoid bit casts for qty
     o.cid = std::stoull(client_id);
@@ -88,7 +138,6 @@ inline static uint8_t validate_fix_values(
     double price, 
     double qty
 ) {
-
     return validate_side(side)
     && validate_order_type(order_type)
     && validate_tif(tif)
